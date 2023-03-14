c1749955878f7278ded0ad68f7283fc02256cb3e
jv-vogler <jvsvogler@gmail.com>
2023-03-14 00:11:13 -0300

    feat: :art: extend tailwind's utilities
    create text-shadow-white for a white glow on text, radius-5, drop-shadow-default

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,20 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
     './src/**/*.{js,ts,jsx,tsx}',
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      borderRadius: {
+        5: '5px',
+      },
+      dropShadow: {
+        default: [
+          '1px 1px 0 rgba(75,85,99,1)',
+          '1px 1px 0 rgba(0,0,0,.2)',
+          '1px 1px 0 rgba(0,0,0,.2)',
+        ],
+        box: ['0 0 2px rgba(255, 255, 255, 1)'],
+      },
+    },
+  },
   plugins: [],
 }
