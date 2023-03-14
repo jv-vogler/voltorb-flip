076b345e9453d09a82cf3228f229c4b1aa621672
jv-vogler <jvsvogler@gmail.com>
2023-03-14 15:56:05 -0300

    feat: :construction: build initial Board UI (wip)

--- src/components/Gameboard.tsx
+++ src/components/Gameboard.tsx
@@ -1,14 +1,53 @@
 import VoltorbFlip from '@/game/VoltorbFlip'
+import Image from 'next/image'
 import React from 'react'
+import Card from './Card'
+import voltorb from '../../public/images/voltorb.png'
 
 type Props = {
-  game: VoltorbFlip
+  game?: VoltorbFlip
 }
 
 const Gameboard = ({ game }: Props) => {
   return (
-    <div>
-      <div></div>
+    <div className="h-96 w-full border-4 border-white bg-[#448563] p-1.5 outline outline-2 outline-gray-600">
+      <div className="flex h-full w-full rounded-xl bg-[#58a66c] p-2">
+        <div className="flex flex-col gap-4">
+          <div className="flex gap-4">
+            <div className="grid grid-cols-5 gap-4">
+              {game?.board.cells.map(row =>
+                row.map((cell, i) => (
+                  <Card key={i}>
+                    {cell.value === 'V' ? (
+                      <Image
+                        src={voltorb}
+                        alt="voltorb"
+                        className="picture-outline"
+                      />
+                    ) : (
+                      cell.value
+                    )}
+                  </Card>
+                ))
+              )}
+            </div>
+            <div className="grid grid-cols-1 gap-4">
+              {game?.board.rowValues.map((row, index) => (
+                <div key={index}>
+                  C: {row.coins} V: {row.voltorbs}
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="flex gap-4">
+            {game?.board.colValues.map((col, index) => (
+              <div key={index}>
+                C: {col.coins} V: {col.voltorbs}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
