a558c75ee6d06634d22ee9f455051d2b476aa629
jv-vogler <jvsvogler@gmail.com>
2023-03-15 22:27:12 -0300

    feat: :sparkles: implement animations and reset after player hits a voltorb

--- src/components/Gameboard.tsx
+++ src/components/Gameboard.tsx
@@ -1,9 +1,9 @@
-import VoltorbFlip from '@/game/VoltorbFlip'
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
-import React from 'react'
 import Card from './Card'
-import voltorb from '../../public/images/voltorb.png'
 import RowColCard from './RowColCard'
+import VoltorbFlip from '@/game/VoltorbFlip'
+import voltorb from '../../public/images/voltorb.png'
 import { indexToCoordinate } from '@/utils/helpers'
 
 type Props = {
@@ -12,6 +12,48 @@ type Props = {
 }
 
 const Gameboard = ({ game, updateGame }: Props) => {
+  const [cardsFlipped, setCardsFlipped] = useState<{ isFlipped: boolean }[]>(
+    game.board.cells.flat().map(cell => ({ isFlipped: cell.isFlipped }))
+  )
+
+  useEffect(() => {
+    setCardsFlipped(() =>
+      game.board.cells.flat().map(cell => ({ isFlipped: cell.isFlipped }))
+    )
+  }, [game.board.cells])
+
+  useEffect(() => {
+    if (game.gameStatus === 'lose') {
+      const columns = [
+        [0, 5, 10, 15, 20],
+        [1, 6, 11, 16, 21],
+        [2, 7, 12, 17, 22],
+        [3, 8, 13, 18, 23],
+        [4, 9, 14, 19, 24],
+      ]
+      setTimeout(() => {
+        setCardsFlipped(() => cardsFlipped.map(() => ({ isFlipped: true })))
+      }, 1000)
+
+      setTimeout(() => {
+        let delay = 0
+        for (let col = 0; col < 5; col++) {
+          setTimeout(() => {
+            setCardsFlipped(prev =>
+              prev.map((card, index) =>
+                columns[col].includes(index) ? { isFlipped: false } : card
+              )
+            )
+          }, delay)
+          delay += 200
+        }
+        setTimeout(() => {
+          updateGame(g => g.restartGame())
+        }, delay + 200)
+      }, 2500)
+    }
+  }, [game.gameStatus])
+
   function handleFlip(row: number, col: number) {
     updateGame(g => {
       g.flipCell(row, col)
@@ -19,7 +61,10 @@ const Gameboard = ({ game, updateGame }: Props) => {
   }
 
   return (
-    <div className="h-96 w-full border-4 border-white bg-[#448563] p-1.5 outline outline-2 outline-gray-600">
+    <div className="relative h-96 w-full border-4 border-white bg-[#448563] p-1.5 outline outline-2 outline-gray-600">
+      {game.gameStatus === 'lose' && (
+        <div className="absolute inset-0 z-50 h-full w-full bg-blue-500 opacity-0"></div>
+      )}
       <div className="flex h-full w-full rounded-xl bg-[#58a66c] p-2">
         <div className="flex flex-col gap-4">
           <div className="flex gap-4">
@@ -29,7 +74,7 @@ const Gameboard = ({ game, updateGame }: Props) => {
                 return (
                   <Card
                     key={i}
-                    isFlipped={cell.isFlipped}
+                    isFlipped={cardsFlipped[i].isFlipped}
                     flipCard={() => handleFlip(coordinate[0], coordinate[1])}
                   >
                     {cell.value === 'V' ? (
@@ -44,6 +89,7 @@ const Gameboard = ({ game, updateGame }: Props) => {
                   </Card>
                 )
               })}
+
               {game.board.colValues.map((col, index) => (
                 <RowColCard
                   coins={col.coins}
